9f7de928ea5ab41ab557c904a0c38f3f42971404
leverpk <kamil.lewandowski7@gmail.com>
2022-01-26 13:49:29 +0100

    thread repository

--- src/main/java/com/example/forumproject/repository/ThreadRepository.java
+++ src/main/java/com/example/forumproject/repository/ThreadRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface ThreadRepository extends JpaRepository<Thread, Long> {
 
-    List<Thread> findAllThreads();
+    List<Thread> findThreadsByUser(String user);
 
     // TODO: zaimplementować repozytorium
 
