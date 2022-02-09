038783788b753963a2e56a4c4c1eb974cc620d77
leverpk <kamil.lewandowski7@gmail.com>
2022-02-09 11:00:39 +0100

    subcategories view done

--- src/main/java/com/example/forumproject/controller/CategoryController.java
+++ src/main/java/com/example/forumproject/controller/CategoryController.java
@@ -1,11 +1,15 @@
 package com.example.forumproject.controller;
 
+import com.example.forumproject.model.Category;
 import com.example.forumproject.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/category")
@@ -30,8 +34,9 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}")
-    public String getSubcategories(Model model, Long id) {
-        model.addAttribute("parentCategories", categoryService.findCategoryByParentId(id));
+    public String getSubcategories(Model model, @PathVariable Long id) {
+        model.addAttribute("parentCategories", categoryService.findCategoryByParentId(null));
+        model.addAttribute("childCategories", categoryService.findCategoryByParentId(id));
         return "/category/subcategories";
     }
 }
