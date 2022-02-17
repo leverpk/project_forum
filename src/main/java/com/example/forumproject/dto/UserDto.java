5e5d2ef392dc5ae830ab761f5939b71ee7675108
leverpk <kamil.lewandowski7@gmail.com>
2022-02-17 15:00:15 +0100

    registration start

--- src/main/java/com/example/forumproject/dto/UserDto.java
+++ src/main/java/com/example/forumproject/dto/UserDto.java
@@ -4,23 +4,17 @@ import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
-
 @Getter
 @Setter
 public class UserDto {
 
     @NotNull
-    @NotEmpty
     private String username;
     @NotNull
-    @NotEmpty
     private String password;
     @NotNull
-    @NotEmpty
     private String matchingPassword;
     @NotNull
-    @NotEmpty
     private String email;
 
 }
